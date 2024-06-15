dfd8258a77d626193b3b60c853292089ddcb64d5
Dup4 <lyuzhi.pan@gmail.com>
2024-06-15 21:58:22 +0800

    chore: rollback
    Signed-off-by: Dup4 <lyuzhi.pan@gmail.com>

--- docker/judgehost/chroot-and-tar.sh
+++ docker/judgehost/chroot-and-tar.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 # Usage: https://github.com/DOMjudge/domjudge/blob/main/misc-tools/dj_make_chroot.in#L58-L87
-/opt/domjudge/judgehost/bin/dj_make_chroot \
-    -D Ubuntu \
-    -R jammy \
-    -i openjdk-17-jdk-headless,openjdk-17-jre-headless
+/opt/domjudge/judgehost/bin/dj_make_chroot
 
 cd /
 echo "[..] Compressing chroot"
